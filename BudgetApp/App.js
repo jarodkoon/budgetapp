f503922a1236147779ef9b56e29283ad4cc00ec7
jarodkoon <jarod.a.koon@outlook.com>
2022-10-24 16:59:02 -0400

    Revert "SQLite"
    This reverts commit 1d2b617c6408bfef3c3b45d45991d6ad8d7eb149.

--- BudgetApp/App.js
+++ BudgetApp/App.js
@@ -5,40 +5,29 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  useColorScheme,
   View,
-  Button
 } from 'react-native';
-import ExpenseItem from './CustomComponents/ExpenseItem'
-
-
-/* SQLite Package */
-import SQLite from 'react-native-sqlite-storage';
-const db = SQLite.openDatabase({
-    name: 'JkBudgetApp',
-    location: 'default'
-  },
-  ()=>{console.log('Successful Connection')},
-  error => {console.log(error)}
-);
-
 
 const App: () => Node = () => {
 
   return (
-    <View style={mainBody.appContainer}>
-      <ExpenseItem></ExpenseItem>
+    <View style={styles.container}>
+      <Text>
+        Hello World
+      </Text>
     </View>
   );
 };
 
-const mainBody = StyleSheet.create({
-  appContainer:{
-    flex: 1,
-    flexDirection: 'column',
+const styles = StyleSheet.create({
+  container: {
+    margin: 0,
+    backgroundColor: 'white',
 
-    marginTop: 50,
-    width: '100%',
-    backgroundColor: 'white'
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
 
